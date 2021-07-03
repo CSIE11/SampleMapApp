f0e67e9f8b66d88217ffcc34b942da549964553c
Hae Chang Lee <dlgockd45@gmail.com>
2021-07-03 15:13:19 +0900

    위치 권한 요청 작업중

--- app/src/main/java/com/hclee/samplemapapp/MapsActivity.kt
+++ app/src/main/java/com/hclee/samplemapapp/MapsActivity.kt
@@ -1,7 +1,16 @@
 package com.hclee.samplemapapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -11,7 +20,21 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    companion object {
+
+        private val REQUIRED_PERMISSIONS = arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        private const val UPDATE_INTERVAL = 1000L
+        private const val FASTEST_UPDATE_INTERVAL = 500L
+        private const val REQUEST_CODE_PERMISSIONS = 1
+    }
+
     private lateinit var googleMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var location: Location
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +42,48 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         (supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment)
                 .getMapAsync(this)
+
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_UPDATE_INTERVAL)
+
+        LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+
+        setDefaultLocation()
+
+        val deniedPermissions = REQUIRED_PERMISSIONS.map { permission ->
+            permission to (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED)
+        }.filter { it.second == false }.map { it.first }.toTypedArray()
+
+        if (deniedPermissions.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, deniedPermissions, REQUEST_CODE_PERMISSIONS)
+        } else {
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            val hasAllGranted = grantResults.all { it == PackageManager.PERMISSION_GRANTED }
+
+            if (hasAllGranted) {
+//                startLocationUpdates()
+            } else {
+
+            }
+        }
+    }
+
+    private fun setDefaultLocation() {
         val seoul = LatLng(37.56, 126.97)
         val markerOptions = MarkerOptions().apply {
             position(seoul)
@@ -29,9 +91,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             snippet("seoul snippet")
         }
 
-        googleMap = map.apply {
-            addMarker(markerOptions)
-            moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 10f))
+        googleMap.also {
+            it.addMarker(markerOptions)
+            it.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 10f))
         }
     }
 }
