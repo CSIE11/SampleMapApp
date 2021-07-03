1787873ca7d3f99d35f7e26b02fbf5dc7d961fb7
Hae Chang Lee <dlgockd45@gmail.com>
2021-07-03 14:29:14 +0900

    구글 맵 연동 로직 추가

--- app/src/main/java/com/hclee/samplemapapp/MapsActivity.kt
+++ app/src/main/java/com/hclee/samplemapapp/MapsActivity.kt
@@ -2,11 +2,18 @@ package com.hclee.samplemapapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class MapsActivity : AppCompatActivity() {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+
     }
 }
